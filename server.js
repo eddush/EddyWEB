037d58ed8cc4b05bc23a79ca25d4026f5e21bf30
eddush <eddyshermant@gmail.com>
2026-09-18 18:45:59 +0300

    add a real drop skin

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(express.static(__dirname));
 
-// נקודת קצה לבדיקת הסיסמה
 app.post('/api/login', (req, res) => {
     const { password } = req.body;
     const adminPassword = process.env.ADMIN_PASSWORD;
 
     if (!adminPassword) {
-        return res.status(500).json({ success: false, message: 'משתנה הסביבה ADMIN_PASSWORD אינו מוגדר בשרת' });
+        return res.status(500).json({ success: false, message: 'משתנה ADMIN_PASSWORD אינו מוגדר' });
     }
 
     if (password === adminPassword) {
@@ -22,13 +21,12 @@ app.post('/api/login', (req, res) => {
     }
 });
 
-// נקודת קצה לשמירת המודעות המעודכנות ב-news.json
 app.post('/api/save-news', (req, res) => {
     const { password, news } = req.body;
     const adminPassword = process.env.ADMIN_PASSWORD;
 
     if (password !== adminPassword) {
-        return res.status(401).json({ success: false, message: 'אין הרשאה מורשית' });
+        return res.status(401).json({ success: false, message: 'אין הרשאה' });
     }
 
     fs.writeFile(path.join(__dirname, 'news.json'), JSON.stringify(news, null, 2), 'utf8', (err) => {
